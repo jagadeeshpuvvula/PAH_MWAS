b1994f3c2eba21e308a77e360eed9865e4ca681e
jagadeeshpuvvula <jagadeesh.puvvula@gmail.com>
2023-03-15 17:28:45 -0400

    update

--- 99_scratch.R
+++ 99_scratch.R
@@ -1,12 +1,9 @@
+dat<- read_csv("~/Documents/MWAS_home/cor_filter/1-Hydroxynapthalene.csv")
+
+res<- read_csv("~/Documents/MWAS_home/pj_results/pah_mom/c18neg/c18neg_mom.csv")|>
+  filter(Variable=="1-Hydroxynapthalene") |>
+  filter(beta_dir %in% c("positive-significant", "negative-significant"))
 
 
-
-
-
-
-
-
-
-
-
-
+filtered_res <- res %>% 
+  inner_join(dat, by = c("mz", "time"))
